42ec06b8def051d025a475db130ea42cc66669b1
Eric Nahas <105933054+EricNahas@users.noreply.github.com>
2023-08-12 14:46:55 -0300

    Add files via upload

---
+++ LP2-tp1-ex1/Teste.java
@@ -0,0 +1,10 @@
+class Teste {
+	public static void main(String[] args) {
+		Author autor = new Author("Armando", "teste123@gmail.com", 'm');
+		System.out.println(autor.getName());
+		System.out.println(autor.getEmail());
+		System.out.println(autor.getGender());
+
+		
+	}
+}
